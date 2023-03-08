5ac9ca66c3355412ab516ec75b9736d7c88a0f32
MarcusV <Marcus-v@outlook.com>
2023-03-08 17:36:56 -0300

    create login and register with private route

--- knowledge_community_app/views.py
+++ knowledge_community_app/views.py
@@ -1,37 +1,71 @@
 from typing import Dict
 from django.contrib import messages
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserLoginForm, UserLoginModelForm, UserRegisterModelForm, UserRegisterForm
+from .models import User
+
+
+LOGGED: bool = False
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'index.html')
+    if LOGGED:
+        return render(request, 'index.html')
+    return redirect('login')
 
 
 def login(request: HttpRequest) -> HttpResponse:
-    form: UserLoginForm = UserLoginForm(request.POST or None)
+    global LOGGED
+    LOGGED = False
+    form: UserLoginModelForm = UserLoginModelForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            check_if_exist = None
+            try:
+                check_if_exist = User.objects.get(email=email)
+                password_if_exits = User.objects.get(password=password)
+                if password == password_if_exits.password and check_if_exist is not None:
+                    LOGGED = True
+                    messages.success(request, "You have been successfully logged!")
+                    return redirect('index')
+            except User.DoesNotExist:
+                messages.warning(request, "User is not registered or incorrect credentials!")
         else:
             messages.error(request,
                            "Fail to login!\nPlease, verify your e-mail or password!")
-    form = UserLoginForm()
+    form2 = UserLoginForm()
     context: Dict[str, UserLoginForm | UserLoginModelForm] = {
-        "form": form
+        "form": form2
     }
     return render(request, 'login.html', context)
 
 
 def register(request: HttpRequest) -> HttpResponse:
-    form: UserRegisterForm = UserRegisterForm(request.POST or None)
+    global LOGGED
+    LOGGED = False
+    form: UserRegisterModelForm = UserRegisterModelForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
-            print(request.POST)
-            messages.success(request, "You have been successfully registered!")
-    form = UserRegisterForm()
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            check_if_exist = None
+            try:
+                check_if_exist = User.objects.get(email=email)
+            except User.DoesNotExist:
+                pass
+            finally:
+                if check_if_exist is None:
+                    new_user = User(name=name, email=email, password=password)
+                    new_user.save()
+                    messages.success(request, "You have been successfully registered!")
+                else:
+                    messages.warning(request, "User already exists!")
+    form2 = UserRegisterForm()
     context: Dict[str, UserRegisterForm | UserRegisterModelForm] = {
-        "form": form
+        "form": form2
     }
     return render(request, 'register.html', context)
